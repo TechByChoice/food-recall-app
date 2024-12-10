2a54808ec2ff6a425f177d9daacfa39e3609bca0
awe <aweful@awews.com>
2024-12-10 18:06:03 -0500

    add mongodb connection using .env file
    show example of mongodb string to connect to db in the cloud

--- api/main.py
+++ api/main.py
@@ -1,6 +1,23 @@
+import os
 from fastapi import FastAPI
+from pymongo import MongoClient
+
+
+
+load_dotenv()
 
 app = FastAPI()
+
+# MongoDB connection
+# mongodb+srv://<username>:<password>@palabras-express-api.whbeh.mongodb.net/?retryWrites=true&w=majority&
+MONGO_URI = os.getenv("MONGO_URI")
+DB_NAME = os.getenv("DB_NAME")
+COLLECTION_NAME = os.getenv
+
+client = MongoClient(MONGO_URI)
+db = client[DB_NAME]
+collection = db[COLLECTION_NAME]
+
 
 @app.get("/")
 def root():
